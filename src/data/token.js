fe945da59ec65eceae56199899ac5d4b0d2d52b6
marRosario <mar.rochy@gmail.com>
2025-12-02 15:20:41 -0300

    Actualizo token, middleware de autenticación y configuración de vercel

--- src/data/token.js
+++ src/data/token.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 
-const secret_key = process.env.JWT_SECRET_KEY;
+const secret_key = process.env.JWT_SECRET_KEY ||"zcue782?jy@dert0pe%d39"
 console.log(secret_key);
 
 export const generateToken = (userData) => {
